1ed0c82a34c7f4aa8cfb27e6afa7bf42fc61e8e7
sapanpatel123 <patelsapan1@gmail.com>
2017-04-17 20:20:06 -0400

    Added Carmel and Diplo ETL scripts

---
+++ etl/Carmel.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import sys
+from datetime import datetime
+import time
+
+f = open("../inputs/other-Carmel_B00256.csv", "r")
+data = f.readlines()
+
+result = data[0]
+
+g = open("../outputs/Carmel.csv", "a+")
+h = open("../outputs/Carmel-Errors", "a+")
+
+for i in range(1, len(data)):
+	element = data[i].split(",")
+	params = {
+		"address": element[2],
+		"key": "AIzaSyDdRxPr_rgoI-z-XVsf94MrM2faF7HV_Jo"
+	}
+	response = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params=params)
+
+	result = response.json()
+
+	if len(result["results"]) == 0:
+		h.write(data[i])
+		print "ERROR: " + element[2]
+		continue
+
+	datetime_object = datetime.strptime(element[0] + " " + element[1], '%m/%d/%Y %H:%M')
+
+	g.write( "\"" + datetime_object.strftime('%m/%d/%Y %H:%M:%S') + "\"," + str(result["results"][0]["geometry"]["location"]["lat"]) + "," + str(result["results"][0]["geometry"]["location"]["lng"]) + ",\"B00256\",\"Carmel\",\"FHV\"\n")
+
+	print "SUCCESS: " + element[2]
+
+g.close()
+h.close()
+
+
---
+++ etl/Diplo.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import sys
+from datetime import datetime
+import time
+
+f = open("../inputs/other-Diplo_B01196.csv", "r")
+data = f.readlines()
+
+result = data[0]
+
+g = open("../outputs/Diplo.csv", "a+")
+h = open("../outputs/Diplo-Errors", "a+")
+
+for i in range(1, len(data)):
+	element = data[i].split(",")
+	params = {
+		"address": element[2],
+		"key": "AIzaSyDdRxPr_rgoI-z-XVsf94MrM2faF7HV_Jo"
+	}
+	response = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params=params)
+
+	result = response.json()
+
+	if len(result["results"]) == 0:
+		h.write(data[i])
+		print "ERROR: " + element[2]
+		continue
+
+	datetime_object = datetime.strptime(element[0] + " " + element[1], '%m/%d/%Y %I:%M:%S %p')
+
+	g.write( "\"" + datetime_object.strftime('%m/%d/%Y %H:%M:%S') + "\"," + str(result["results"][0]["geometry"]["location"]["lat"]) + "," + str(result["results"][0]["geometry"]["location"]["lng"]) + ",\"B01196\",\"Diplo\",\"FHV\"\n")
+
+	print "SUCCESS: " + element[2]
+
+g.close()
+h.close()
+
+
